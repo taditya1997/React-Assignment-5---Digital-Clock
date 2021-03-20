afea54ad810c82b82342455ca3ba9a36c9b5c0c0
Aditya <taditya1997@gmail.com>
2021-03-20 13:30:09 +0530

    Initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,43 @@
-import React, {Component, useState} from "react";
+import React, {Component, useState,useEffect} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+function App()
+{
+    const[time,setTime]=useState('');
+    const[am,setampm]=useState('PM');
 
-        return(
-            <>
-               
-            </>
-        )
+    const timer= ()=>
+    {
+        var clock= new Date();
+        var hours=clock.getHours();
+        var minutes=clock.getMinutes();
+        var seconds=clock.getSeconds();
+        hours<12?setampm("AM"):setampm("PM");
+        if(hours==0)
+        {
+            hour=12;
+            setampm('AM');
+        }
+        if(hours>12)
+        {
+            hours=hours%12;
+            setampm("PM");
+        }
+        setTime(hours+":"+minutes+":"+seconds+" "+am); 
+        
     }
+    useEffect(()=>{setInterval(timer,1000)},[])
+
+    const digitalClock=()=>
+    {
+        return(
+            <div className="Clock"><h3>{time}</h3></div>
+        )
+            
+    }
+return(
+    <>
+    {digitalClock()}
+    </>)
 }
-
-
 export default App;
